359cd12c86f48c1a3c3545dc8706b8ff4faccb74
system sylvere BARINDA <systembarinda@gmail.com>
2026-08-18 20:12:02 +0200

    done

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,9 @@
-import {defineConfig} from "vite"; import react from "@vitejs/plugin-react";
-export default defineConfig({plugins:[react()],server:{port:5173,proxy:{"/api":"http://localhost:5000","/images":"http://localhost:5000"}}});
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 5173,
+  },
+});
